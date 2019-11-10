5005087cc8101df70cc9231c8e2d3135ed35d5fa
Javierd <Javierd299@gmail.com>
2019-11-10 23:26:47 +0100

    Turn getTopMonths and getTopVentas into functions

--- Practica3/getTopMonths.sql
+++ Practica3/getTopMonths.sql
@@ -1,4 +1,14 @@
-SELECT DATE_PART('year', orderdate) as year, DATE_PART('month', orderdate) as month, SUM(quantity) as productos, SUM(price) as importe 
-FROM orderdetail NATURAL JOIN orders
-GROUP BY year, month
-HAVING SUM(quantity)>10000 OR SUM(price)>100000;
+CREATE OR REPLACE FUNCTION getTopMonths(minQuantity NUMERIC, minPrice NUMERIC)
+    RETURNS TABLE(year DOUBLE PRECISION, month DOUBLE PRECISION, productos BIGINT, importe NUMERIC)
+AS $$
+    DECLARE
+    BEGIN
+
+
+        RETURN QUERY SELECT DATE_PART('year', orderdate) as year, DATE_PART('month', orderdate) as month, SUM(quantity) as productos, SUM(price) as importe 
+            FROM orderdetail NATURAL JOIN orders
+            GROUP BY year, month
+            HAVING SUM(quantity)>=minQuantity OR SUM(price)>=minPrice
+            ORDER BY year, month;
+    END;
+$$ LANGUAGE plpgsql;
